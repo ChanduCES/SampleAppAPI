174550803ae74c17880eb2903d8811001077b8ef
CESLTD\chandu.chandran <chandu.chandran@cesltd.com>
2023-07-19 21:43:42 +0530

    Initial app

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using SampleApp.Models;
+using SampleApp.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var connectionString = builder.Configuration.GetConnectionString("EmployeeDB");
+builder.Services.AddDbContext<EmployeeContext>(x => x.UseSqlServer(connectionString));
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
---
+++ SampleApp/Repository/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using SampleApp.Models;
+
+namespace SampleApp.Repository
+{
+    public interface IEmployeeRepository
+    {
+        Task<List<EmployeeModel>> GetAllEmployees(EmployeeQueryParameters parameters);
+        Task<EmployeeModel> GetEmployeeById(Guid id);
+        Task<EmployeeModel> AddEmployee(EmployeeModel employeeModel);
+        Task<EmployeeModel> UpdateEmployee(EmployeeModel employeeModel);
+        Task<bool> RemoveEmployee(EmployeeModel employee);
+    }
+}
